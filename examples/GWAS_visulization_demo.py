5c0020e203e46c8e8bd3c9ba669af52d3287cadd
Xiuqi Pan <xp76@cornell.edu>
2024-04-24 15:50:10 -0400

    rename sample annot file for further inclusion of more sample annots

--- examples/GWAS_visulization_demo.py
+++ examples/GWAS_visulization_demo.py
@@ -8,7 +8,7 @@ from scripts.GWAS_visualizer import main
 
 if __name__ == "__main__":
     args = argparse.Namespace(gwas_summary_bed_path="sample_data/sample.gwas_summary.bed6",
-                              annotation_bed_path=["sample_data/sample.annot.bed3"],
+                              annotation_bed_path=["sample_data/sample1.annot.bed3"],
                               annotation_names=["sample_annotation"],
                               plot_chr="chr2",
                               plot_start_loc=127100000,
