a1525b6a4f339a854c5a97cdb0599d21f888fee0
Xiuqi Pan <xp76@cornell.edu>
2024-06-12 14:03:53 -0400

    Add test scripts for BedTablePairEnd and base BedTable3
    * test cases for the new class BedTablePairEnd
    * test cases for new methods for BedTable3
        - test_concat
        - test_region_search
        - test_get_region_by_index

--- tests/RGTools/BedTableTest.py
+++ tests/RGTools/BedTableTest.py
@@ -6,7 +6,10 @@ import os
 import numpy as np
 import pandas as pd
 
-from scripts.RGTools.BedTable import BedTable3, BedTable6, BedTable6Plus
+from scripts.RGTools.BedTable import BedTable3, \
+                                     BedTable6, \
+                                     BedTable6Plus, \
+                                     BedTablePairEnd
 
 class TestBedTable(unittest.TestCase):
     # Public methods for testing BedTable
@@ -33,6 +36,9 @@ class TestBedTable(unittest.TestCase):
                                      )
     
     def setUp(self) -> None:
+        '''
+        Set up the test directory and data file path.
+        '''
         super().setUp()
         self.data_dir = "bedTableTest_temp_data"
         if not os.path.exists(self.data_dir):
@@ -40,6 +46,9 @@ class TestBedTable(unittest.TestCase):
         self.data_file = os.path.join(self.data_dir, "test.bed")
 
     def tearDown(self) -> None:
+        '''
+        Clean up test data directory.
+        '''
         super().tearDown()
         if os.path.exists(self.data_dir):
             shutil.rmtree(self.data_dir)
@@ -152,12 +161,39 @@ class TestBedTable3(TestBedTable):
         end_locs = bed_table.get_end_locs()
 
         self.assertArrayEqual(end_locs, self.data_df["end"].values)
+    
+    def test_get_region_by_index(self):
+        bed_table = self.__init_test_bed_table()
+
+        region = bed_table.get_region_by_index(0)
+
+        self.assertArrayEqual(region.values, self.data_df.loc[0].values)
+
+        region = bed_table.get_region_by_index(np.array([1,3]))
+
+        self.assertArrayEqual(region.values, self.data_df.loc[[1,3]].values)
 
     def test_iter_regions(self):
         bed_table = self.__init_test_bed_table()
         start_locs = np.array([r["start"] for r in bed_table.iter_regions()])
 
         self.assertArrayEqual(start_locs, self.data_df["start"].values)
+    
+    def test_concat(self):
+        bed_table = self.__init_test_bed_table()
+
+        new_bed_table = BedTable3()
+        new_bed_table.load_from_dataframe(pd.DataFrame({
+            "chrom": ["chr1", "chr1", "chr2"],
+            "start": [2, 80, 50],
+            "end": [6, 120, 70],}), 
+            )
+
+        result_bed_table = bed_table.concat(new_bed_table)
+
+        self.assertArrayEqual(result_bed_table.subset_by_index(np.array([1, 3, 6])).to_dataframe().values,
+                              new_bed_table.to_dataframe().values,
+                              )
 
     def test_sort(self):
         bed_table = BedTable3()
@@ -168,6 +204,42 @@ class TestBedTable3(TestBedTable):
         self.assertArrayEqual(bed_table.to_dataframe().values,
                               self.data_df.sort_values(["chrom", "start"]).values,
                               )
+    
+    def test_search_region(self):
+        bed_table = self.__init_test_bed_table()
+
+        # Perfect match
+        chrom = "chr2"
+        start = 3
+        end = 7
+
+        region_idx = bed_table.search_region(chrom, start, end, 
+                                             overlapping_base=4,
+                                             )
+
+        self.assertArrayEqual(region_idx, np.array([2]))
+
+        # Non-perfect match
+        chrom = "chr2"
+        start = 5
+        end = 12
+
+        region_idx = bed_table.search_region(chrom, start, end,
+                                             overlapping_base=2,
+                                             )
+
+        self.assertArrayEqual(region_idx, np.array([2, 3]))
+
+        # No match
+        chrom = "chr2"
+        start = 9
+        end = 12
+
+        region_idx = bed_table.search_region(chrom, start, end,
+                                             overlapping_base=1,
+                                             )
+        
+        self.assertArrayEqual(region_idx, np.array([]))
 
     def __init_test_bed_table(self) -> BedTable3:
         bed_table = BedTable3()
@@ -380,6 +452,154 @@ class TestBedTable6Plus(TestBedTable):
 
     # TODO: add test for:
     # * __force_dtype
+
+class TestBedTablePairEnd(TestBedTable):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.data_df = pd.DataFrame({
+            "chrom": ["chr1", "chr1", "chr2", "chr2"],
+            "start": [1, 8, 3, 4],
+            "end": [5, 12, 7, 8],
+            "chrom2": ["chr1", "chr1", "chr2", "chr3"],
+            "start2": [6, 13, 8, 5],
+            "end2": [7, 14, 9, 8],
+            "name": ["name1", "name2", "name3", "name4"],
+            "score": [0.1, 0.2, 0.3, 0.4],
+            "strand": ["+", "-", "+", "-"],
+            "strand2": ["-", "+", "-", "+"],
+            "extra_str_field": ["extra1", "extra2", "extra3", "extra4"],
+            "extra_int_field": [1, 2, 3, 4],
+        })
+
+        self.extra_field_names = ["extra_str_field", "extra_int_field"]
+        self.extra_field_dtype = [str, int]
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_get_other_region_chroms(self):
+        bed_table = self.__init_test_bed_table()
+
+        chrom2 = bed_table.get_other_region_chroms()
+
+        self.assertArrayEqual(chrom2, self.data_df["chrom2"].values)
+    
+    def test_get_other_region_starts(self):
+        bed_table = self.__init_test_bed_table()
+
+        start2 = bed_table.get_other_region_starts()
+
+        self.assertArrayEqual(start2, self.data_df["start2"].values)
+
+    def test_get_other_region_ends(self):
+        bed_table = self.__init_test_bed_table()
+
+        end2 = bed_table.get_other_region_ends()
+
+        self.assertArrayEqual(end2, self.data_df["end2"].values)
+
+    def test_get_pair_names(self):
+        bed_table = self.__init_test_bed_table()
+
+        pair_names = bed_table.get_pair_names()
+
+        self.assertArrayEqual(pair_names, self.data_df["name"].values)
+
+    def test_get_pair_scores(self):
+        bed_table = self.__init_test_bed_table()
+
+        pair_scores = bed_table.get_pair_scores()
+
+        self.assertArrayEqual(pair_scores, self.data_df["score"].values)
+
+    def test_get_region_strands(self):
+        bed_table = self.__init_test_bed_table()
+
+        strands = bed_table.get_region_strands()
+
+        self.assertArrayEqual(strands, self.data_df["strand"].values)
+    
+    def test_get_other_region_strands(self):
+        bed_table = self.__init_test_bed_table()
+
+        strands2 = bed_table.get_other_region_strands()
+
+        self.assertArrayEqual(strands2, self.data_df["strand2"].values)
+
+    def test_get_extra_column(self):
+        bed_table = self.__init_test_bed_table()
+
+        for extra_field_name, extra_field_dtype in zip(self.extra_field_names, self.extra_field_dtype):
+            extra_field_data = bed_table.get_extra_column(extra_field_name)
+
+            self.assertArrayEqual(extra_field_data, self.data_df[extra_field_name].values)
+
+    def test_search_pair_extra_column(self):
+        bed_table = self.__init_test_bed_table()
+
+        # Perfect match
+        extra_str_val = bed_table.search_pair_extra_column(chr1="chr1",
+                                                           start1=1,
+                                                           end1=5,
+                                                           chr2="chr1",
+                                                           start2=6,
+                                                           end2=7,
+                                                           column_name="extra_str_field",
+                                                           overlapping_base1=4, 
+                                                           overlapping_base2=1, 
+                                                           )
+        
+        self.assertEqual(extra_str_val.max(), "extra1")
+
+        # Non-perfect match
+        extra_str_val = bed_table.search_pair_extra_column(chr1="chr1",
+                                                           start1=1,
+                                                           end1=6,
+                                                           chr2="chr1",
+                                                           start2=6,
+                                                           end2=7,
+                                                           column_name="extra_str_field",
+                                                           overlapping_base1=5, 
+                                                           overlapping_base2=1,
+                                                           )
+
+        self.assertArrayEqual(extra_str_val, np.array([]))
+
+        extra_str_val = bed_table.search_pair_extra_column(chr1="chr1",
+                                                           start1=1,
+                                                           end1=5,
+                                                           chr2="chr1",
+                                                           start2=6,
+                                                           end2=7,
+                                                           column_name="extra_int_field",
+                                                           overlapping_base1=1, 
+                                                           overlapping_base2=1, 
+                                                           )
+
+        self.assertEqual(extra_str_val.max(), 1)
+
+        # cross-chromosomal match
+        extra_int_val = bed_table.search_pair_extra_column(chr1="chr2",
+                                                           start1=6,
+                                                           end1=7,
+                                                           chr2="chr3",
+                                                           start2=7,
+                                                           end2=8,
+                                                           column_name="extra_int_field",
+                                                           overlapping_base1=1, 
+                                                           overlapping_base2=1, 
+                                                           )
+        
+        self.assertEqual(extra_int_val.max(), 4)
+
+    def __init_test_bed_table(self):
+        bed_table = BedTablePairEnd(extra_column_names=self.extra_field_names,
+                                    extra_column_dtype=self.extra_field_dtype,
+                                    )
+        bed_table.load_from_dataframe(self.data_df.copy())
+
+        return bed_table
 
 if __name__ == "__main__":
     unittest.main()
