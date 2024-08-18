b203d11bc5f0d5041fed25b7fe0dda61cf7a0080
Xiuqi Pan <xp76@cornell.edu>
2024-08-18 15:51:18 -0400

    Update count_table_tools to have general purpose per_million_normalization

--- scripts/count_table_tool.py
+++ scripts/count_table_tool.py
@@ -3,39 +3,66 @@
 # downstream processing of count tables
 
 import argparse
+import sys
 
 import numpy as np
 import pandas as pd
 
-def set_parser(parser):
-    parser.add_argument("--inpath", "-I", 
-                        help="Input path for count table.", 
-                        required=True, 
-                        dest="inpath", 
-                        )
-    
-    parser.add_argument("--cpm_opath", 
-                        help="Output path for CPM table.", 
-                        default=None, 
-                        dest="cpm_opath", 
-                        )
+class CountTableTool:
+    @staticmethod
+    def main(args):
+        if args.subcommand == "per_million_normalization":
+            CountTableTool.per_million_normalization_main(args)
+        else:
+            raise ValueError("Invalid subcommand.")
 
-def count_df2cpm_df(count_df):
-    CPM_table = count_df / count_df.sum(axis=0) * 1e6
-    return CPM_table
+    @staticmethod
+    def set_parser(parser):
+        subparsers = parser.add_subparsers(dest="subcommand")
+
+        parser_per_million_normalization = subparsers.add_parser("per_million_normalization", 
+                                                                 help="Per million normalization.", 
+                                                                 )
+
+        CountTableTool.set_parser_general_io(parser_per_million_normalization)
+
+    @staticmethod
+    def set_parser_general_io(parser):
+        parser.add_argument("--inpath", "-I", 
+                            help="Input path for count table.", 
+                            required=True, 
+                            dest="inpath", 
+                            )
+        
+        parser.add_argument("--opath", 
+                            help="Output path.", 
+                            default="stdout", 
+                            dest="opath", 
+                            )
+
+    @staticmethod
+    def read_input_df(input_path):
+        return pd.read_csv(input_path, 
+                           index_col=0, 
+                           )
+
+    @staticmethod
+    def write_output_df(output_df, opath):
+        if opath == "stdout":
+            output_df.to_csv(sys.stdout)
+        else:
+            output_df.to_csv(opath)
+
+    @staticmethod
+    def per_million_normalization_main(args):
+        input_df = CountTableTool.read_input_df(args.inpath)
+        output_df = input_df / input_df.sum(axis=0) * 1e6
+        CountTableTool.write_output_df(output_df, args.opath)
+        return None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Count Table Tool.")
 
-    set_parser(parser)
-
+    CountTableTool.set_parser(parser)
     args = parser.parse_args()
-
-    count_df = pd.read_csv(args.inpath, 
-                           index_col=0, 
-                           )
-
-    if args.cpm_opath:
-        CPM_table = count_df2cpm_df(count_df)
-        CPM_table.to_csv(args.cpm_opath)
-
+    CountTableTool.main(args)
