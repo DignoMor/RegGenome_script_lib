17659c0ef538211ec419b62bf7fe54452c23e4dd
Xiuqi Pan <xp76@cornell.edu>
2025-01-22 15:17:23 -0500

    Change gnomicelement_tool permission

--- scripts/genomicelement_tool.py
+++ scripts/genomicelement_tool.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+#!/usr/bin/env python
 
 import argparse
 import sys
@@ -19,45 +19,45 @@ class GenomicElementTool:
                                                 )
 
         GenomicElementTool.set_parser_count_bw(parser_count_bw)
-    
+
     @staticmethod
     def set_parser_count_bw(parser):
         GenomicElements.set_parser_genomic_element_region(parser)
-        parser.add_argument("--bw_pl", 
+        parser.add_argument("--bw_pl",
                             help="Plus strand bigwig file.",
                             required=True,
-                            type=str, 
+                            type=str,
                             )
-        
-        parser.add_argument("--bw_mn", 
+
+        parser.add_argument("--bw_mn",
                             help="Minus strand bigwig file.",
-                            type=str, 
+                            type=str,
                             default=None,
                             )
-        
-        parser.add_argument("--single_bw", 
+
+        parser.add_argument("--single_bw",
                             help="If there is only plus strand bigwig file.",
-                            type=str2bool, 
+                            type=str2bool,
                             default=False,
                             )
-        
+
         parser.add_argument("--ignore_strandness",
                             help="Ignore strandness of the signal.",
-                            type=str2bool, 
+                            type=str2bool,
                             default=False,
                             )
-        
-        parser.add_argument("--quantification_type", 
+
+        parser.add_argument("--quantification_type",
                             help="Type of quantification.",
-                            type=str, 
+                            type=str,
                             default="raw_count",
                             choices=BwTrack.get_supported_quantification_type(),
                             )
-        
-        parser.add_argument("--opath", 
+
+        parser.add_argument("--opath",
                             help="Output path for counting.",
                             required=True,
-                            type=str, 
+                            type=str,
                             )
 
     @staticmethod
@@ -66,28 +66,28 @@ class GenomicElementTool:
             GenomicElementTool.count_bw_main(args)
         else:
             raise ValueError("Unknown subcommand: {}".format(args.subcommand))
-    
+
     @staticmethod
     def count_bw_main(args):
-        genomic_elements = GenomicElements(region_path=args.region_file_path, 
-                                           region_file_type=args.region_file_type, 
-                                           genome_path=args.genome_path, 
+        genomic_elements = GenomicElements(region_path=args.region_file_path,
+                                           region_file_type=args.region_file_type,
+                                           genome_path=args.genome_path,
                                            )
         region_bt = genomic_elements.get_region_bed_table()
 
-        bw_track = BwTrack(bw_pl_path=args.bw_pl, 
-                           bw_mn_path=args.bw_mn, 
-                           single_bw=args.single_bw, 
+        bw_track = BwTrack(bw_pl_path=args.bw_pl,
+                           bw_mn_path=args.bw_mn,
+                           single_bw=args.single_bw,
                            )
-        
+
         output_list = []
         for region in region_bt.iter_regions():
-            output_list.append(bw_track.count_single_region(region["chrom"], 
-                                                            region["start"], 
-                                                            region["end"], 
-                                                            region["strand"], 
+            output_list.append(bw_track.count_single_region(region["chrom"],
+                                                            region["start"],
+                                                            region["end"],
+                                                            region["strand"],
                                                             output_type=args.quantification_type,
-                                                            ), 
+                                                            ),
                                )
         output_arr = np.array(output_list)
 
