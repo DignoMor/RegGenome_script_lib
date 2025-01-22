85a9351b09b7eee5cf9da893c74dd7216cfcf25c
Xiuqi Pan <xp76@cornell.edu>
2025-01-22 12:10:39 -0500

    Add Genomic Element Tool with count bw function

---
+++ scripts/genomicelement_tool.py
@@ -0,0 +1,100 @@
+#/usr/bin/env python3
+
+import argparse
+import sys
+
+import numpy as np
+
+from RGTools.GenomicElements import GenomicElements
+from RGTools.BwTrack import BwTrack
+from RGTools.utils import str2bool
+
+class GenomicElementTool:
+    @staticmethod
+    def set_parser(parser):
+        subparsers = parser.add_subparsers(dest="subcommand")
+
+        parser_count_bw = subparsers.add_parser("count_bw",
+                                                help="Count signal in bigwig files.",
+                                                )
+
+        GenomicElementTool.set_parser_count_bw(parser_count_bw)
+    
+    @staticmethod
+    def set_parser_count_bw(parser):
+        GenomicElements.set_parser_genomic_element_region(parser)
+        parser.add_argument("--bw_pl", 
+                            help="Plus strand bigwig file.",
+                            required=True,
+                            type=str, 
+                            )
+        
+        parser.add_argument("--bw_mn", 
+                            help="Minus strand bigwig file.",
+                            type=str, 
+                            default=None,
+                            )
+        
+        parser.add_argument("--single_bw", 
+                            help="If there is only plus strand bigwig file.",
+                            type=str2bool, 
+                            default=False,
+                            )
+        
+        parser.add_argument("--ignore_strandness",
+                            help="Ignore strandness of the signal.",
+                            type=str2bool, 
+                            default=False,
+                            )
+        
+        parser.add_argument("--quantification_type", 
+                            help="Type of quantification.",
+                            type=str, 
+                            default="raw_count",
+                            choices=BwTrack.get_supported_quantification_type(),
+                            )
+        
+        parser.add_argument("--opath", 
+                            help="Output path for counting.",
+                            required=True,
+                            type=str, 
+                            )
+
+    @staticmethod
+    def main(args):
+        if args.subcommand == "count_bw":
+            GenomicElementTool.count_bw_main(args)
+        else:
+            raise ValueError("Unknown subcommand: {}".format(args.subcommand))
+    
+    @staticmethod
+    def count_bw_main(args):
+        genomic_elements = GenomicElements(region_path=args.region_file_path, 
+                                           region_file_type=args.region_file_type, 
+                                           genome_path=args.genome_path, 
+                                           )
+        region_bt = genomic_elements.get_region_bed_table()
+
+        bw_track = BwTrack(bw_pl_path=args.bw_pl, 
+                           bw_mn_path=args.bw_mn, 
+                           single_bw=args.single_bw, 
+                           )
+        
+        output_list = []
+        for region in region_bt.iter_regions():
+            output_list.append(bw_track.count_single_region(region["chrom"], 
+                                                            region["start"], 
+                                                            region["end"], 
+                                                            region["strand"], 
+                                                            output_type=args.quantification_type,
+                                                            ), 
+                               )
+        output_arr = np.array(output_list)
+
+        np.save(args.opath, output_arr)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Genomic element tool.")
+    GenomicElementTool.set_parser(parser)
+    args = parser.parse_args()
+    GenomicElementTool.main(args)
---
+++ tests/GenomicElementToolTest.py
@@ -0,0 +1,113 @@
+
+import unittest
+import argparse
+import shutil
+import sys
+import os
+
+import pandas as pd
+import numpy as np
+
+from scripts.RGTools.BedTable import BedTable3, BedTable6, BedTable6Plus
+
+sys.path.append('scripts')
+from scripts.genomicelement_tool import GenomicElementTool
+
+class GenomicElementToolTest(unittest.TestCase):
+    def setUp(self):
+        self.__temp_dir = "GenomicElementToolTest_temp_data"
+
+        if not os.path.exists(self.__temp_dir):
+            os.makedirs(self.__temp_dir)
+
+        self.setup_count_bw_test()
+        
+        return super().setUp()
+
+    def setup_count_bw_test(self):
+        self.__bw_pls = ["sample_data/ENCFF993VCR.pl.bigWig"]
+        self.__bw_mns = ["sample_data/ENCFF182TPF.mn.bigWig"]
+
+        self.__bed3_path = os.path.join(self.__temp_dir, "test.bed3")
+        self.__bed6_path = os.path.join(self.__temp_dir, "test.bed6")
+        self.__bed6gene_path = os.path.join(self.__temp_dir, "test.bed6gene")
+
+        region_df = pd.DataFrame({"chr": ["chr6", "chr14", "chr17"],
+                                  "start_loc": [170553801, 75278325, 45894026],
+                                  "end_loc": [170554802, 75279326, 45895027],
+                                  "name": ["TBP", "FOS", "MAPT"],
+                                  "score": [1, 2, 3],
+                                  "strand": ["+", "-", "+"],
+                                  "gene_name": ["gene1", "gene2", "gene3"],
+                                  }, 
+                                  )
+
+        # Write regions in different bed format
+        bedtable3 = BedTable3()
+        bedtable3.load_from_dataframe(region_df[["chr", "start_loc", "end_loc"]], 
+                                      column_map={"chrom": "chr", 
+                                                  "start": "start_loc", 
+                                                  "end": "end_loc", 
+                                                  },
+                                      )
+        bedtable3.write(self.__bed3_path)
+
+        bedtable6 = BedTable6()
+        bedtable6.load_from_dataframe(region_df[["chr", "start_loc", "end_loc", "name", "score", "strand"]], 
+                                      column_map={"chrom": "chr", 
+                                                  "start": "start_loc", 
+                                                  "end": "end_loc", 
+                                                  "name": "name", 
+                                                  "score": "score", 
+                                                  "strand": "strand", 
+                                                  },
+                                      )
+        bedtable6.write(self.__bed6_path)
+
+        bedtable6gene = BedTable6Plus(extra_column_names=["gene_name"],
+                                      extra_column_dtype=[str],
+                                      )
+        bedtable6gene.load_from_dataframe(region_df, 
+                                          column_map={"chrom": "chr", 
+                                                      "start": "start_loc", 
+                                                      "end": "end_loc", 
+                                                      "name": "name", 
+                                                      "score": "score", 
+                                                      "strand": "strand", 
+                                                      "gene_name": "gene_name", 
+                                                      },
+                                          )
+        bedtable6gene.write(self.__bed6gene_path)
+
+    def tearDown(self):
+        if os.path.exists(self.__temp_dir):
+            shutil.rmtree(self.__temp_dir)
+
+        return super().tearDown()
+    
+    def get_count_bw_single_args(self):
+        args = argparse.Namespace()
+        args.subcommand = "count_bw"
+        args.bw_pl = self.__bw_pls[0]
+        args.bw_mn = self.__bw_mns[0]
+        args.single_bw = False
+        args.region_file_path = self.__bed6_path
+        args.region_file_type = "bed6"
+        args.genome_path = None
+        args.quantification_type = "raw_count"
+        args.opath = os.path.join(self.__temp_dir, "output.npy")
+
+        return args
+
+    def test_count_bw(self):
+        args = self.get_count_bw_single_args()
+
+        GenomicElementTool.count_bw_main(args)
+
+        output = np.load(args.opath)
+
+        self.assertEqual(output.shape, (3,))
+        self.assertEqual(output[0], 17)
+        self.assertEqual(output[2], 348)
+
+
