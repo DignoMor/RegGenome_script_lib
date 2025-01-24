6147ed7680dd7a0e7b97a835113546f16b325f7f
Xiuqi Pan <xp76@cornell.edu>
2025-01-24 14:08:50 -0500

    Update exogeneous metaplot

--- examples/plot_exogeneous_metaplot.sh
+++ examples/plot_exogeneous_metaplot.sh
@@ -5,8 +5,9 @@ OPATH=wdir/plot_exogeneous_metaplot
 mkdir -p ${OPATH}
 
 scripts/exogeneous_tool.py metaplot \
-    --inpath ${INPATH}/sample.exogeneous.chr2.fa \
-    --region_path ${INPATH}/sample.exogeneous.chr2.predicted_peaks.bed \
+    --fasta ${INPATH}/sample.exogeneous.chr2.fa \
+    --region_file_path ${INPATH}/sample.exogeneous.chr2.predicted_peaks.bed \
+    --region_file_type bed3 \
     --outpath ${OPATH}/sample.png \
     --signal_tracks_pl ${INPATH}/sample.exogeneous.chr2.pred_profiles_pl.npy \
     --signal_tracks_mn ${INPATH}/sample.exogeneous.chr2.pred_profiles_mn.npy
