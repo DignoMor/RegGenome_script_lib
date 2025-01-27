249fd0073cc1307fd8463c83e429f33d9c91a132
Xiuqi Pan <xp76@cornell.edu>
2025-01-26 22:07:22 -0500

    Add compare_mutagenesis to exogeneous tool

--- scripts/exogeneous_tool.py
+++ scripts/exogeneous_tool.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import sys
 import re
+import os
 
 import numpy as np
 import pandas as pd
@@ -12,7 +14,8 @@ import matplotlib.patches as patches
 
 from Bio import SeqIO
 
-from RGTools.BedTable import BedTable3
+from RGTools.utils import NumpyEncoder
+from RGTools.BedTable import BedTable3, BedTable6Plus
 from RGTools.ExogeneousSequences import ExogeneousSequences
 
 class ExogeneousTool:
@@ -30,6 +33,12 @@ class ExogeneousTool:
                                               help="Filter exogeneous sequence by a regular expression.",
                                               )
         ExogeneousTool.set_parser_filter(parser_filter)
+
+        parser_compare_mutagenesis = subparsers.add_parser("compare_mutagenesis",
+                                                           help="Compare Mutagenesis Results.",
+                                                           )
+        
+        ExogeneousTool.set_parser_compare_mutagenesis(parser_compare_mutagenesis)
 
     @staticmethod
     def set_parser_metaplot(parser):
@@ -74,6 +83,41 @@ class ExogeneousTool:
                             required=True,
                             )
 
+    @staticmethod
+    def set_parser_compare_mutagenesis(parser):
+        ExogeneousSequences.set_parser_exogeneous_sequences(parser)
+        parser.add_argument("--regex_ref_seqs", 
+                            help="Regular expression for reference sequences. "
+                                 "Must contain one capture group that captures sequence ID.",
+                            required=True,
+                            )
+        
+        parser.add_argument("--regex_mut_seqs",
+                            help="Regular expression for mutated sequences. "
+                                 "Must contain one capture group that captures sequence ID.",
+                            required=True,
+                            )
+        
+        parser.add_argument("--pl_track_npy", 
+                            help="Numpy file for plus strand signal.", 
+                            required=True,
+                            )
+
+        parser.add_argument("--mn_track_npy", 
+                            help="Numpy file for minus strand signal.", 
+                            required=True,
+                            )
+        
+        parser.add_argument("--total_count_plot_path", 
+                            help="Path to the total count plot.",
+                            default=None, 
+                            )
+        
+        parser.add_argument("--opath", 
+                            help="Output path for the results written in json format.",
+                            required=True,
+                            )
+        
     @staticmethod
     def load_fasta(fasta_path):
         '''
@@ -170,6 +214,138 @@ class ExogeneousTool:
         y_max = track2plot.max() if track2plot.max() > 0 else 0
 
         ax.set_ylim(y_min, y_max)
+    
+    @staticmethod
+    def match_ref_mut_regex(input_bt, ref_regex, mut_regex):
+        '''
+        Match reference and mutated sequences with regex
+        
+        Keyword arguments:
+        - input_bt: input bed table
+        - ref_regex: regex for reference sequences
+        - mut_regex: regex for mutated sequences
+
+        Returns:
+        - result_bt: result *unsorted* BedTable6Plus object, 
+                     with extra columns seq_id and seq_type.
+                     seq_id is the identifier that groups ref and 
+                     mut seqs together, while seq_type is either
+                     ref or mut.
+        '''
+        region_bt_w_anno = BedTable6Plus(extra_column_names=["seq_id", "seq_type"], 
+                                         extra_column_dtype=[str, str],
+                                         enable_sort=False, 
+                                         )
+        
+        region_chr_list = []
+        region_start_list = []
+        region_end_list = []
+        region_seq_id_list = []
+        region_seq_type_list = []
+
+        for region in input_bt.iter_regions():
+            seq_name = region["chrom"]
+
+            ref_match = re.match(ref_regex, seq_name)
+            mut_match = re.match(mut_regex, seq_name)
+
+            if ref_match:
+                region_chr_list.append(region["chrom"])
+                region_start_list.append(region["start"])
+                region_end_list.append(region["end"])
+                region_seq_id_list.append(ref_match.group(1))
+                region_seq_type_list.append("ref")
+
+            elif mut_match:
+                region_chr_list.append(region["chrom"])
+                region_start_list.append(region["start"])
+                region_end_list.append(region["end"])
+                region_seq_id_list.append(mut_match.group(1))
+                region_seq_type_list.append("mut")
+
+            else:
+                raise ValueError("Sequence name {} does not match any regex.".format(seq_name))
+        
+        region_df_w_anno = pd.DataFrame({"chrom": region_chr_list,
+                                         "start": region_start_list,
+                                         "end": region_end_list,
+                                         "name": ".", 
+                                         "score": ".", 
+                                         "strand": ".",
+                                         "seq_id": region_seq_id_list,
+                                         "seq_type": region_seq_type_list,
+                                         }, 
+                                        columns=region_bt_w_anno.column_names,
+                                        )
+        
+        region_bt_w_anno.load_from_dataframe(region_df_w_anno)
+
+        return region_bt_w_anno
+
+    @staticmethod
+    def quantify_track_diff(pl_tracks_ref, mn_tracks_ref, 
+                            pl_tracks_mut, mn_tracks_mut):
+        '''
+        Quantify the difference between reference and mutated tracks
+        
+        Keyword arguments:
+        - pl_tracks_ref: plus strand tracks for reference sequences
+        - mn_tracks_ref: minus strand tracks for reference sequences
+        - pl_tracks_mut: plus strand tracks for mutated sequences
+        - mn_tracks_mut: minus strand tracks for mutated sequences
+
+        Returns:
+        - track_info_dict: dictionary containing track information
+        '''
+        track_info_dict = {}
+
+        assert pl_tracks_ref.shape == mn_tracks_ref.shape
+        assert pl_tracks_mut.shape == mn_tracks_mut.shape
+        assert pl_tracks_ref.shape[0] == 1
+
+        ref_log_total_count = np.log10(pl_tracks_ref.sum() - mn_tracks_ref.sum())
+        mut_log_total_count = np.log10(pl_tracks_mut.sum(axis=1) - mn_tracks_mut.sum(axis=1))
+        diff_log_total_count = mut_log_total_count - ref_log_total_count
+
+        track_info_dict["ref_log_total_count"] = ref_log_total_count
+        track_info_dict["mut_log_total_count"] = mut_log_total_count
+        track_info_dict["diff_log_total_count"] = abs(diff_log_total_count)
+
+        return track_info_dict
+    
+    @staticmethod
+    def plot_mutated_vs_ref_predicted_counts(ref_counts, mut_counts, opath=None):
+        '''
+        Plot the mutated vs. reference predicted counts
+        
+        Keyword arguments:
+        - ref_counts: np.array of reference counts
+        - mut_counts: np.array of mutated counts
+        - opath: output path for the plot
+
+        Returns:
+        - None
+        '''
+        fig, ax = plt.subplots(1,1, 
+                               figsize=(8,8), 
+                               )
+
+        x_y_max = max(ref_counts.max(), mut_counts.max())
+        ax.plot([0, x_y_max], [0, x_y_max], color="r", linestyle="--")
+
+        ax.scatter(ref_counts, mut_counts, 
+                   s=abs(ref_counts - mut_counts)*200,
+                   color="k", 
+                   )
+
+        ax.set_xlim(0, x_y_max)
+        ax.set_ylim(0, x_y_max)
+        ax.set_xlabel("Reference Log Total Count")
+        ax.set_ylabel("Mutated Log Total Count")
+        ax.set_title("Mutated vs. Reference Predicted Counts")
+
+        if opath:
+            fig.savefig(opath)
 
     @staticmethod
     def metaplot_main(args):
@@ -233,12 +409,66 @@ class ExogeneousTool:
                         SeqIO.write(record, output_f, "fasta")
 
     @staticmethod
+    def compare_mutagenesis_main(args):
+        exogeneous_seq = ExogeneousSequences(args.region_file_path, 
+                                             args.region_file_type, 
+                                             args.fasta,
+                                             )
+
+        region_bt = exogeneous_seq.get_region_bed_table()
+        region_bt_w_anno = ExogeneousTool.match_ref_mut_regex(region_bt,
+                                                              args.regex_ref_seqs,
+                                                              args.regex_mut_seqs,
+                                                              )
+        
+        pl_tracks = np.load(args.pl_track_npy)
+        mn_tracks = np.load(args.mn_track_npy)
+
+        seq_id_list = []
+        diff_dict_list = []
+        for seq_id in np.unique(region_bt_w_anno.get_region_extra_column("seq_id")):
+            ref_logical = (region_bt_w_anno.get_region_extra_column("seq_id") == seq_id) & \
+                (region_bt_w_anno.get_region_extra_column("seq_type") == "ref")
+            ref_logical = np.array(ref_logical, dtype=bool)
+            mut_logical = (region_bt_w_anno.get_region_extra_column("seq_id") == seq_id) & \
+                (region_bt_w_anno.get_region_extra_column("seq_type") == "mut")
+            mut_logical = np.array(mut_logical, dtype=bool)
+            
+            track_info_dict = ExogeneousTool.quantify_track_diff(pl_tracks[ref_logical, :], 
+                                                                 mn_tracks[ref_logical, :],
+                                                                 pl_tracks[mut_logical, :],
+                                                                 mn_tracks[mut_logical, :],
+                                                                 )
+        
+            seq_id_list.append(seq_id)
+            diff_dict_list.append(track_info_dict)
+
+            with open(os.path.join(args.opath, seq_id + ".json"), "w") as output_f:
+                json.dump(track_info_dict, output_f, cls=NumpyEncoder)
+
+        if args.total_count_plot_path:
+            ref_counts = np.array([d["ref_log_total_count"] for d in diff_dict_list])
+
+            mut_counts = []
+            for d in diff_dict_list:
+                largest_diff_ind = np.argmax(d["diff_log_total_count"])
+                mut_counts.append(d["mut_log_total_count"][largest_diff_ind])
+            
+            ExogeneousTool.plot_mutated_vs_ref_predicted_counts(np.array(ref_counts), 
+                                                                np.array(mut_counts), 
+                                                                opath=args.total_count_plot_path,
+                                                                )
+
+    @staticmethod
     def main(args):
         if args.subcommand == "metaplot":
             ExogeneousTool.metaplot_main(args)
 
         elif args.subcommand == "filter":
             ExogeneousTool.filter_main(args)
+
+        elif args.subcommand == "compare_mutagenesis":
+            ExogeneousTool.compare_mutagenesis_main(args)
 
         else:
             raise NotImplementedError
--- tests/ExogeneousToolTest.py
+++ tests/ExogeneousToolTest.py
@@ -2,6 +2,7 @@
 import argparse
 import unittest
 import shutil
+import json
 import sys
 import os
 
@@ -20,6 +21,8 @@ class ExogeneousToolTest(unittest.TestCase):
         self.__sample_fasta_path = "large_sample_data/hg38.fa"
         self.__sample_exogeneous_fasta_path = "sample_data/sample_exogeneous_sequence/sample.exogeneous.chr2.fa"
         self.__sample_bed3_path = "sample_data/sample_exogeneous_sequence/sample.exogeneous.chr2.predicted_peaks.bed"
+        self.__sample_pl_track_npy_path = "sample_data/sample_exogeneous_sequence/sample.exogeneous.chr2.pred_profiles_pl.npy"
+        self.__sample_mn_track_npy_path = "sample_data/sample_exogeneous_sequence/sample.exogeneous.chr2.pred_profiles_mn.npy"
 
         return super().setUp()
     
@@ -55,3 +58,33 @@ class ExogeneousToolTest(unittest.TestCase):
             for record in SeqIO.parse(output_f, "fasta"):
                 self.assertEqual(record.id[:24], "chr2_127105185_127107299")
                 self.assertEqual(record.seq[:5], "CAAAG")
+
+    def get_compare_mutagenesis_default_args(self):
+        args = argparse.Namespace(fasta=self.__sample_exogeneous_fasta_path,
+                                  region_file_path=self.__sample_bed3_path,
+                                  region_file_type="bed3",
+                                  regex_ref_seqs="(.*)_ref", 
+                                  regex_mut_seqs="(.*)_[0-9]*:[ATCG]2[ATCG]",
+                                  pl_track_npy=self.__sample_pl_track_npy_path, 
+                                  mn_track_npy=self.__sample_mn_track_npy_path,
+                                  opath=self.__test_dir, 
+                                  total_count_plot_path=os.path.join(self.__test_dir, "total_count_plot.png"),
+                                  )
+        
+        return args
+    
+    def test_compare_mutagenesis_main(self):
+        args = self.get_compare_mutagenesis_default_args()
+        
+        ExogeneousTool.compare_mutagenesis_main(args)
+
+        with open(os.path.join(args.opath, "chr2_127104997_127107111.json"), "r") as output_f:
+            ref_mut_dict = json.load(output_f)
+            self.assertEqual(ref_mut_dict["ref_log_total_count"], 2.607356071472168)
+            self.assertEqual(ref_mut_dict["mut_log_total_count"], 
+                             [2.607358455657959, 
+                              2.6025302410125732, 
+                              2.6089253425598145, 
+                              2.606109619140625, 
+                              ], 
+                             )
