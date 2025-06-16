9b5ce95118f84fe2d0b7f38f4d12b506cb3bbdff
Xiuqi Pan <xp76@cornell.edu>
2025-06-16 16:03:34 -0400

    Add split_fragments script and corresponding tests

---
+++ scripts/split_fragments.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+import argparse
+
+import pandas as pd
+import numpy as np
+
+class SplitFragments:
+    @staticmethod
+    def set_parser(parser):
+        parser.add_argument("--fragments", 
+                            type=str, 
+                            required=True, 
+                            help="Fragments file", 
+                            )
+
+        parser.add_argument("--label_file", 
+                            type=str, 
+                            required=True, 
+                            help="Label file.", 
+                            )
+
+        parser.add_argument("--oheader", 
+                            type=str, 
+                            required=True, 
+                            help="Output file header.", 
+                            )
+
+    @staticmethod
+    def main(args):
+        fragment_df = pd.read_csv(args.fragments, 
+                                  sep="\t", 
+                                  names=["chrom", "start", "end", "barcode", "support"], 
+                                  )
+
+        label_df = pd.read_csv(args.label_file, 
+                               sep="\t", 
+                               names=["cell_type"], 
+                               index_col=0, 
+                               )
+        
+        label_dict = {k:v for k, v in zip(label_df.index, label_df["cell_type"])}
+
+        for cell_type in label_df.loc[:, "cell_type"].unique():
+            cell_type_logical_list = []
+            for barcode in fragment_df["barcode"]:
+                if barcode in label_dict.keys():
+                    cell_type_logical_list.append(label_dict[barcode] == cell_type)
+                else:
+                    cell_type_logical_list.append(False)
+
+            cell_type_df = fragment_df.loc[np.array(cell_type_logical_list)]
+            cell_type_df.to_csv(args.oheader + "." + cell_type + ".fragments.tsv", 
+                                sep="\t", 
+                                index=False, 
+                                header=False,
+                                )
+                
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    SplitFragments.set_parser(parser)
+    args = parser.parse_args()
+    SplitFragments.main(args)
---
+++ tests/SplitFragmentsTest.py
@@ -0,0 +1,85 @@
+
+import os
+import gzip
+import shutil
+import argparse
+import unittest
+import requests
+
+import pandas as pd
+
+from scripts.split_fragments import SplitFragments
+
+
+class SplitFragmentsTest(unittest.TestCase):
+    def setUp(self):
+        self.__test_dir = "SplitFragmentsTest"
+        os.makedirs(self.__test_dir, exist_ok=True)
+
+        self.__fragments_file = os.path.join(self.__test_dir, "fragments.tsv")
+        self.__label_file = os.path.join(self.__test_dir, "labels.tsv")
+
+        self.__download_example_data()
+    
+    def __download_example_data(self):
+        url = "https://personal.broadinstitute.org/bjames/AD_snATAC/fragments/D19-8612_fragments.bed.gz"
+        response = requests.get(url)
+        with open(self.__fragments_file + ".gz", "wb") as f:
+            f.write(response.content)
+        
+        fragment_df = pd.read_csv(self.__fragments_file + ".gz", 
+                                  sep="\t", 
+                                  compression="gzip",
+                                  header=None,
+                                  )
+        fragment_df.to_csv(self.__fragments_file, 
+                           sep="\t", 
+                           index=False, 
+                           header=False,
+                           )
+        os.remove(self.__fragments_file + ".gz")
+
+        url = "https://personal.broadinstitute.org/bjames/AD_snATAC/integration/integration.tsv.gz"
+        response = requests.get(url)
+        with open(self.__label_file + ".temp.tsv.gz", "wb") as f:
+            f.write(response.content)
+
+        label_df = pd.read_csv(self.__label_file + 
+                               ".temp.tsv.gz", 
+                               sep="\t", 
+                               compression="gzip",
+                               )
+        label_df = label_df.iloc[:, [0, 6]]
+        label_df.iloc[:, 0] = label_df.iloc[:, 0].agg(lambda x: x.split("#")[-1])
+        label_df.iloc[:, 1].replace("Opc", "OPC", inplace=True)
+        label_df.to_csv(self.__label_file, 
+                        sep="\t", 
+                        index=False,
+                        header=False,
+                        )
+
+        os.remove(self.__label_file + ".temp.tsv.gz")
+
+
+    def tearDown(self):
+        shutil.rmtree(self.__test_dir)
+        super().tearDown()
+
+    def test_split_fragments(self):
+        args = argparse.Namespace(
+            fragments=self.__fragments_file,
+            label_file=self.__label_file,
+            oheader=os.path.join(self.__test_dir, "split_fragments"),
+        )
+
+        SplitFragments.main(args)
+
+        exc_df = pd.read_csv(os.path.join(self.__test_dir, "split_fragments.Exc.fragments.tsv"), 
+                             sep="\t", 
+                             header=None, 
+                             )
+        self.assertEqual(exc_df.shape[0], 1669846)
+        self.assertEqual(exc_df.shape[1], 5)
+
+if __name__ == "__main__":
+    unittest.main()
